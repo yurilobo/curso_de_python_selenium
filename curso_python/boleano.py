34c44a548bc40effeb0700feadf64bd4b1fdd97d
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-08-25 22:18:49 -0300

    Boleano

---
+++ curso_python/boleano.py
@@ -0,0 +1,16 @@
+2>3
+3>2
+
+type(True)
+nome_1 = 'Fausto'
+idade_1=3
+nome_2 = 'Maria'
+idade_2=23
+nome_3 = 'Hugo'
+idade_3=35
+nome_1 ==nome_2
+nome_1==nome_3
+
+nome_2 != nome_1
+
+nome_1!=nome_2 and idade_1!=idade_2
