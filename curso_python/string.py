3fa942b774857898cf8dde7c8547297b5fbf01c6
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-08-26 10:48:03 -0300

    estrudo de string

---
+++ curso_python/string.py
@@ -0,0 +1,15 @@
+texto = """
+oi meu nome é
+yuri
+Estou em constante estudo sobe pogramação
+"""
+
+minha_mensagem = 'Olá Aline, você é muito legal'
+
+outra_mensagem = 'Você tem quantos anos?'
+
+# print(minha_mensagem + '. ' + outra_mensagem)
+
+print('*' * 20)
+print('*** Meu Programa ***')
+print('*' * 20)
