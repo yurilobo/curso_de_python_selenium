e0ea1828d34b70a673f94ca26006f88931aab670
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-08-25 22:34:19 -0300

    condicionais if elif e else

---
+++ curso_python/if.py
@@ -0,0 +1,7 @@
+resposta =input('Papai você comprou pão?')
+if resposta =='sim':
+    print('Obrigado papai')
+elif resposta =='nao':
+    print('Estou chorando')
+else:
+    print(' Vou dormir ')
---
+++ curso_python/if2.py
@@ -0,0 +1,5 @@
+resposta =input('Bruno você gosta mais de rust ou de python?')
+if resposta =='python':
+    print('Toca ai meu irmao')
+else:
+    print('Tudo bem desde que nao seja Java ')
